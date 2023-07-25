9d4b1fba4d6e4903da0f992f60c8edada11cba98
Aviya <aviyariven@gmail.com>
2023-07-25 13:09:21 +0300

    DB connection

---
+++ DB_Access/DB_connection.py
@@ -0,0 +1,15 @@
+import pymysql
+
+# MySQL database connection
+connection = pymysql.connect(
+    host='sql9.freesqldatabase.com',
+    user='sql9635075',
+    password="picCFnW2dC",
+    db='sql9635075',
+)
+
+
+
+
+
+
